8c1ddd3e643e2f40de5426e0997640d2bd3605c7
prestance1 <oudrhiri.amin@gmail.com>
2022-10-06 21:30:35 +0100

    :lotus_position: Solved

--- primes.py
+++ primes.py
@@ -1,6 +1,19 @@
+from typing import List
+
+def _gen_prime() -> int:
+    curr_num = 2
+    while True:
+        if all(
+           (curr_num % n != 0) for n in range(2, curr_num) 
+        ):
+            yield curr_num
+        curr_num += 1
+
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
-def primes(number_of_primes):
-    list = []
-    return list
+def primes(number_of_primes: int) -> List[int]:
+    if number_of_primes < 1:
+        raise ValueError("Invalid argument")
+    prime_generator = _gen_prime()
+    return [next(prime_generator) for _ in range(number_of_primes)]
